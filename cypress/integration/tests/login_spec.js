bd3528115860d49eecbefd4c8df88ec95f633938
angstrohbeck <65632307+angstrohbeck@users.noreply.github.com>
2020-11-18 18:58:56 -0500

    Add files via upload

---
+++ cypress/integration/tests/login_spec.js
@@ -0,0 +1,10 @@
+describe('Negative Login Test', () => {
+    it('login scenario', () => {
+        cy.visit('http://automationpractice.com/index.php')
+        cy.get('.login').click()
+        cy.get('#email').type('cypresstesting2020@gmail.com')
+        cy.get('#passwd').type('jobhunt20')
+        cy.get('#SubmitLogin > span').click()
+        cy.title().should('include','My Account')
+    });
+});
